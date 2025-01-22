6ab5dbdc90aecda361fd62a0842e0c94b2e6a64c
DapKr <daphnaz129@gmail.com>
2025-01-22 03:06:45 -0500

    added practice task to restaurant menu

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak- $25', 'Pasta- $18', 'Burger- $20', 'Salmon- $19.5'];
+const dessertMenu = ['Cake- $7', 'Ice Cream- $5', 'Pudding- $6', 'Fruit Salad- $4'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
