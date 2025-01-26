3602ba935d17eb3f78782d3c64accaa1388fd85c
DapKr <daphnaz129@gmail.com>
2025-01-26 03:43:21 -0500

    added practice task to speed type analysis lab

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -36,9 +36,13 @@ button.onclick = endTest;
                 wpm = Math.round((typedWords / timeElapsed) * 60);
             }
 
+            //Check length of string
+            var lengthOfText = userTypedText.length;
+
             // Display the results
             var outputDiv = document.getElementById("output");
             outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+                "<p>Total Length: " + lengthOfText + "</p>" +
                 "<p>Words Typed: " + typedWords + "</p>" +
                 "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
                 "<p>Words Per Minute (WPM): " + wpm + "</p>";
